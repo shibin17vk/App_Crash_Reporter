ebb6754746d25a7f5252e2d6067f19a79fe12fc3
Shibin VK <shibin17vk@gmail.com>
2015-08-18 15:50:22 +0530

    Update Receiver.java

--- crashReporterLibrary/src/main/java/com/blog4android/crashreporter/Receiver.java
+++ crashReporterLibrary/src/main/java/com/blog4android/crashreporter/Receiver.java
@@ -20,30 +20,5 @@ public class Receiver extends BroadcastReceiver {
 				
 	}
 
-	private class ReceiverSuport extends AsyncTask{
-
-		private Context context;
-		
-		public ReceiverSuport(Context context){
-			this.context=context;
-		}
-		
-		@Override
-		protected Object doInBackground(Object... params) {
-			
-			int pendingRepotrs=Persistent.getPendinRepotrsCount(context);
-			 
-			if(pendingRepotrs>0){
-			
-				Intent dataIntent=new Intent(context, ServiceReportCrash.class);
-				dataIntent.putExtra(Utilities.INTENT_KEY.SRC,Utilities.INTENT_KEY.SRC_RECEIVER);			
-				context.startService(dataIntent);
-				
-			}
-			return null;
-		}
-		
-		
-	}
 	
 }
